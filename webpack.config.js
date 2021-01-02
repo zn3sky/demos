6921510635afafd11aba7a9e9fb189c99d26954e
zn3sky <zn3sky@gmail.com>
2021-01-03 00:09:11 +0900

    mod: conf

--- webpack.config.js
+++ webpack.config.js
@@ -2,20 +2,13 @@ const webpack = require('webpack');
 const path = require('path');
 
 module.exports = {
-    // モードの設定。指定可能な値は、none, development ,production（デフォルト）
-    // https://webpack.js.org/concepts/mode/#mode-development
-    mode: 'development',
-    // アプリケーションが実行を開始されるポイント(エントリーポイント)
-    // 配列で指定すると、すべての項目が実行される
-    // https://webpack.js.org/configuration/entry-context/#entry
+    mode: 'development',// TODO
     entry: {
         index: './src/index.js',
         list: './src/list.js',
       },
     output: {
         filename: '[name].bundle.js',
-        // ビルド後のファイルが出力される"絶対パス"ディレクトリ
-        // https://webpack.js.org/configuration/output/#outputpath
         path: path.join(__dirname, 'dist/js')
     },
     optimization: {
