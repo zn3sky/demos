86d6473c7ff81ef48be0f7276f476fc45856ed70
zn3sky <zn3sky@gmail.com>
2021-01-03 00:55:57 +0900

    add: glob

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,40 @@
 const webpack = require('webpack');
 const path = require('path');
+const glob = require("glob");
 
-module.exports = {
-    mode: 'development',// TODO
-    entry: {
-        index: './src/index.js',
-        list: './src/list.js',
-      },
-    output: {
-        filename: '[name].bundle.js',
-        path: path.join(__dirname, 'dist/js')
-    },
-    optimization: {
-        splitChunks: {
-            name: 'vendor',
-            chunks: 'initial',
-            minChunks: 1
+module.exports = (env, argv) => {
+    //const IS_DEVELOPMENT = argv.mode === 'development';
+
+    const srcDir = "./src";
+    const entries = glob.sync(
+        "**/*.js", {
+            ignore: "**/_*.js",
+            cwd: srcDir,
         }
-    },
-    plugins: [
-        new webpack.ProvidePlugin({
-          $: 'jquery'
-        })
-    ]
+    )
+    .map(function (key) {
+        return [key, path.resolve(srcDir, key)];
+    });
+
+    const entryObjects = Object.fromEntries(entries);
+
+    return {
+        entry: entryObjects,
+        output: {
+            filename: '[name]',
+            path: path.join(__dirname, 'dist/js')
+        },
+        optimization: {
+            splitChunks: {
+                name: 'vendor.js',
+                chunks: 'initial',
+                minChunks: 10
+            }
+        },
+        plugins: [
+            new webpack.ProvidePlugin({
+                $: 'jquery'
+            })
+        ]
+    }
 };
